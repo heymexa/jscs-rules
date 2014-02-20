d4197757b0c4de5453065dd4435c985fb9a30f12
Mikhail.Voronov <mikhail.voronov@alpari.org>
2014-02-20 15:33:36 +0400

    advanced configuration of functions

--- src/require-functions-content.js
+++ src/require-functions-content.js
@@ -10,21 +10,8 @@ module.exports.prototype = {
             typeof option === 'object',
             'requireFunctionsContent option requires object value'
         );
-        assert(
-            Array.isArray(option.functionNames),
-            'property requireFunctionsContent.functionNames requires array value'
-        );
-        assert(
-            typeof option.regexpText === 'string' && new RegExp(option.regexpText),
-            'property requireFunctionsContent.regexpText requires string value'
-        );
 
-        this._functionNames = {};
-        for (var i = 0, l = option.functionNames.length; i < l; i++) {
-            this._functionNames[option.functionNames[i]] = true;
-        }
-
-        this._regexpText = option.regexpText;
+        this._functions = option;
     },
 
     getOptionName: function () {
@@ -33,17 +20,17 @@ module.exports.prototype = {
 
     check: function (file, errors) {
         var fileSource = file.getSource(),
-            functionNames = this._functionNames,
-            regexpText = this._regexpText;
+            functions = this._functions;
         file.iterateNodesByType([ 'FunctionDeclaration', 'FunctionExpression' ], function (node) {
             if (node.parentNode.key && node.parentNode.key.name) {
-                var functionName = node.parentNode.key.name;
-                if (functionNames[functionName]) {
+                var functionName = node.parentNode.key.name,
+                    functionOptions = functions[functionName];
+                if (functionOptions) {
                     var range = node.parentNode.value.body.range,
                         functionBody = fileSource.substring(range[0], range[1]),
-                        regexp = new RegExp(regexpText.replace(/{functionName}/g, functionName));
+                        regexp = new RegExp(functionOptions.regexpText.replace(/{functionName}/g, functionName));
                     if (!regexp.test(functionBody)) {
-                        errors.add('require functions content', node.loc.start.line, 0);
+                        errors.add(functionOptions.error, node.loc.start.line, 0);
                     }
                 }
             }
